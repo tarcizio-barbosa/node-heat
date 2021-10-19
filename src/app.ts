ca0b2ef68af593e2559ba20147517777a8427363
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-10-19 20:19:31 -0300

    create a ws event emit on create message service

--- src/app.ts
+++ src/app.ts
@@ -37,4 +37,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => console.log('Server is running on port 4000 😎'));
+export { serverHttp, io };
