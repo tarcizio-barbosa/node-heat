b71f3c036447c4699059b4aac90c88f490c99588
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-10-19 20:49:06 -0300

    create get user profile feature and finish the project

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLasThreeMessagesController } from './controllers/GetLasThreeMessagesController';
+import { ProfileUserController } from './controllers/ProfileUserController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
@@ -18,5 +19,7 @@ router.get(
   '/messages/getlastthree',
   new GetLasThreeMessagesController().handle
 );
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
