0bd06c6166de78ee91543190d04851e396770b69
Tarcizio B <tarcizio.barbosa@outlook.com>
2021-10-19 19:40:00 -0300

    add socket.io and cors on app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected on socket ${socket.id}`);
+});
 
 app.use(express.json());
 
